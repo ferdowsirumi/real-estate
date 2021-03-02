8cfe7d4243819eaaf44972903ed963e9d5793deb
Altaf Hussain <Altaf.Hussain@rci.rogers.ca>
2021-03-01 21:04:56 -0500

    added Specification

--- detail.js
+++ detail.js
@@ -1,15 +1,11 @@
-console.log(window.location.search);
-
+//Load and view large image
+/* Hazera Ferdowsi : ID: 301168815 */
 var urlParams = new URLSearchParams(window.location.search);
-
-var imageUrl = "/img/1.png";
-var imageName = '1.png';
-if(urlParams.has('img'))
-{
+var imageUrl = "";
+var imageName = 'phone121.png';
+if (urlParams.has('img')) {
     imageName = urlParams.get('img');
 }
-
 imageUrl = "img/" + imageName;
 var imgDetailsImage = document.getElementById('detailsImage');
 imgDetailsImage.src = imageUrl;
-console.log(imageUrl);
